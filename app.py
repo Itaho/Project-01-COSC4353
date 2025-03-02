36b7db04cbfbac0f7ddc7f85f0a56179e52be84d
Everest Nguyen <45898852+Itaho@users.noreply.github.com>
2025-03-02 13:58:44 -0600

    Update app.py

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ DB_PASS = os.getenv("DB_PASS", "Moosefactory123")
 DB_NAME = os.getenv("DB_NAME", "moosefactory_sql")
 
 # Microsoft Azure App credentials
-CLIENT_ID = os.getenv("MS_CLIENT_ID", "YOUR_CLIENT_ID")
-CLIENT_SECRET = os.getenv("MS_CLIENT_SECRET", "YOUR_CLIENT_SECRET")
-AUTHORITY = os.getenv("MS_AUTHORITY", "https://login.microsoftonline.com/YOUR_TENANT_ID")
+CLIENT_ID = os.getenv("MS_CLIENT_ID", "e6db8f7f-6fb1-4845-9d9d-df9f7b68cbb7")
+CLIENT_SECRET = os.getenv("MS_CLIENT_SECRET", "5c7bfd46-2852-4781-83c8-d28be25ab885")
+AUTHORITY = os.getenv("MS_AUTHORITY", "https://login.microsoftonline.com/170bbabd-a2f0-4c90-ad4b-0e8f0f0c4259")
 REDIRECT_PATH = "/auth/microsoft/callback"  # Must match the redirect URI in Azure
 SCOPE = ["User.Read"]  # Permissions to request
 
