c25a97b757849fd22dcd188bbccc84523e1a9418
Phat Chau <164248138+pnchau@users.noreply.github.com>
2025-04-02 19:08:18 -0500

    Delete startup.sh

--- startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /app
-# Use PORT env var if available, default to 8000 otherwise
-export PORT=${PORT:-8000}
-# Start the app with Gunicorn
-exec gunicorn --bind 0.0.0.0:$PORT app:app
