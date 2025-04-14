79ce35aa80bf1880f04482ddaccd3337ed83060f
Phat Chau <164248138+pnchau@users.noreply.github.com>
2025-04-14 16:04:58 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-gunicorn --bind=0.0.0.0:8000 --timeout 600 app:app
+# Install LaTeX + make
+apt-get update && apt-get install -y make texlive-latex-base texlive-latex-extra
+
+# Optionally install more packages
+
+# Then start your app
+gunicorn app:app --bind=0.0.0.0:8000
