53a3a58b609872a8700f9e91d846c4ef8cffade8
Phat Chau <164248138+pnchau@users.noreply.github.com>
2025-04-14 18:23:58 -0500

    Delete startup.sh

--- startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Install LaTeX + make
-apt-get update && apt-get install -y make texlive-latex-base texlive-latex-extra
-
-# Optionally install more packages
-
-# Then start your app
-gunicorn app:app --bind=0.0.0.0:8000
